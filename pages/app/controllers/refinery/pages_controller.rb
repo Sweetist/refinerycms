a25cf86d982f81234c3cc9e60110e0eab2d0d543
Brice Sanchez <contact@brice-sanchez.com>
2017-07-07 23:47:30 -0400

    Now we use redirect 301 to show pages
    instead of 302. See
    http://api.rubyonrails.org/classes/ActionController/Redirecting.html#met
    hod-i-redirect_to

--- pages/app/controllers/refinery/pages_controller.rb
+++ pages/app/controllers/refinery/pages_controller.rb
@@ -25,11 +25,11 @@ module Refinery
     #
     def show
       if should_skip_to_first_child?
-        redirect_to refinery.url_for(first_live_child.url) and return
+        redirect_to refinery.url_for(first_live_child.url), status: 301 and return
       elsif page.link_url.present?
-        redirect_to page.link_url and return
+        redirect_to page.link_url, status: 301 and return
       elsif should_redirect_to_friendly_url?
-        redirect_to refinery.url_for(page.url), :status => 301 and return
+        redirect_to refinery.url_for(page.url), status: 301 and return
       end
 
       render_with_templates?
@@ -61,6 +61,10 @@ module Refinery
 
     def current_user_can_view_page?
       page.live? || authorisation_manager.allow?(:plugin, "refinery_pages")
+    end
+
+    def current_refinery_user_can_access?(plugin)
+      admin? && authorisation_manager.allow?(:plugin, plugin)
     end
 
     def first_live_child
